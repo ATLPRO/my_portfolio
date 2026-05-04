ccadf6daa189e3f326a49cd49c371157735bdcb4
ATLPRO <aguedelea2003@gmail.com>
2026-05-05 00:27:58 +0100

    fis type and imports

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_EMAILJS_TEMPLATE_ID: string
   readonly VITE_EMAILJS_PUBLIC_KEY: string
 }
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
